f46c3902beed4f1ecfa476cda391879fe725fe9e
YoungEun Park <oeun.park@gmail.com>
2021-10-18 21:55:56 -0400

    draft

--- app/src/main/java/com/example/myapplication/InfoActivity.java
+++ app/src/main/java/com/example/myapplication/InfoActivity.java
@@ -1,6 +1,63 @@
 package com.example.myapplication;
 
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InfoActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnDisplay, btnReturn;
+    EditText editTextName, editTextAge;
+
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_info);
+
+        initialize();
+    }
+
+    private void initialize()
+    {
+        btnDisplay = findViewById(R.id.btnDisplay);
+        btnReturn = findViewById(R.id.btnReturn);
+
+        //active LISTENER for the buttons
+        btnDisplay.setOnClickListener(this);
+        btnReturn.setOnClickListener(this);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextAge = findViewById(R.id.editTexAge);
+    }
+@Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnDisplay:
+            {
+                String name = editTextName.getText().toString();
+                String age = editTextAge.getText().toString();
+
+                //just for dev see internally what is going on
+                Log.d("DISPLAY","Name"+name);
+
+                String info = "Name " + name + "Age: " + age;
+
+                Toast.makeText(this.info, Toast.LENGTH_SHORT).show();
+
+                break;
+
+            }
+
+            case R.id.btnReturn: {
+                finish();
+                break;
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void infStudent (View view){
+        Intent intent = new Intent(this, InfoActivity.class);
+        startActivity(intent);
     }
 }
